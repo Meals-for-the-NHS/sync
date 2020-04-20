e86986ef44572dee0124a0c7fb74aa1fd298b448
ThomasMattDean <thomas@matthewdean.co.uk>
2020-04-20 18:14:26 +0100

    fake sync added

--- functions/src/fake_sync.ts
+++ functions/src/fake_sync.ts
@@ -1,31 +1,37 @@
-import * as guardian from './guardian'
-import * as scotland from './scotland'
-import { Table, Donation, DonationSummary, AirtableRecord, TableUpdateData } from './types'
-
-
+import {TableUpdateData} from "./types";
+import * as guardian from "./guardian";
+import * as wales from "./wales";
+import * as scotland from "./scotland";
 
 export async function cases() {
   const newData: TableUpdateData = {}
-  const scotsCases = await scotland.casesScotland()
-  Object.entries(scotsCases).forEach(([la, _cases]) => {
-    newData[la] = {
-      'Cumulative Cases': _cases
-    }
-  })
   const casesByLA = await guardian.casesByLocalAuthority()
   Object.entries(casesByLA).forEach(([la, _cases]) => {
     newData[la] = {
       'Cumulative Cases': _cases
     }
   })
-  console.log(newData)
+
+  const casesByLAWales = await wales.casesWales()
+  Object.entries(casesByLAWales).forEach(([la, _cases]) => {
+    newData[la] = {
+      'Cumulative Cases': _cases
+    }
+  })
+
+  const casesByLAScotland = await scotland.casesScotland()
+  Object.entries(casesByLAScotland).forEach(([la, _cases]) => {
+    newData[la] = {
+      'Cumulative Cases': _cases
+    }
+  })
+  //
+  // return airtable.updateTable({
+  //   tableName: 'Cases (All hospitals)',
+  //   lookupField: 'Local Authority',
+  //   updateFields: ['Cumulative Cases'],
+  //   newData
+  // })
 
 
-//   return airtable.updateTable({
-//     tableName: 'Cases (All hospitals)',
-//     lookupField: 'Local Authority',
-//     updateFields: ['Cumulative Cases'],
-//     newData
-//   })
 }
-
